73c3622c4a829e490bc700827e12d72f7ba747c6
Lucas-AguiarDev <jlucasaguiar2001@gmail.com>
2022-02-22 14:55:38 -0300

    Upload do Projeto

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Hotsite.Models;
+
+namespace Hotsite.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Cadastrar(Interesse cad)
+        {
+            try
+            {
+                InteresseService dbs = new InteresseService();
+                dbs.CadastraInteresse(cad);
+
+                return Json(new {status = "OK"});
+            }
+            catch (Exception e)
+            {
+                _logger.LogError("Erro ao conectar com Banco de Dados" + e.Message);
+
+                return Json(new {status = "ERR", mensagem = "Falha no banco de dados!"});
+            }
+
+        }
+
+    }
+}
---
+++ wwwroot/js/site.js
@@ -0,0 +1,56 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+function Cadastrar() {
+
+    //array com dados do form
+    let parametros = {
+        Nome: $("#nome").val(),
+        Email: $("#email").val(),
+        Mensagem: $("#mensagem").val()
+    };
+
+    console.log("Linha 14" + parametros);
+
+
+    //requisicao via POST informando o endereco e array de parametros
+    $.post("/Home/Cadastrar", parametros)
+
+        .done(
+            function (data) {
+
+                if (data.status === "OK") 
+                {
+                    $("#modalCad").after("<p>NewsLetter assinado com sucesso!</p>");
+                    $("#modalCad").hide();
+                }
+                else 
+                {
+                    $("#modalCad").after("<p>" + data.mensagem + "</p>");
+                }
+            }
+        )
+
+        .fail(
+            function () 
+            {
+                alert("Ocorreu um erro");
+            }
+        );
+}
+
+
+$(document).ready(
+
+    function () {
+        $("#frmCadastro").submit(
+
+            function (e) {
+                e.preventDefault();
+                Cadastrar();
+            }
+        );
+    }
+);
